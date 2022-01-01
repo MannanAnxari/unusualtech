cfd6176cdea3d4d32e0ca94dfe3683014024003c
MannanAnxari <87243359+MannanAnxari@users.noreply.github.com>
2022-01-02 01:27:19 +0500

    Sample

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-input", {
+    strings:["Abdul-Mannan", "Software Engineer", "a Web Developer", "Android Developer"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
